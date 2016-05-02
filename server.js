5883569f0228f5b7e9ecb2018e7ecfbdf871cf07
Paul Cutting <paul.cutting@axonvibe.com>
2016-05-02 20:00:33 +0100

    Add front-end form to submit message

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ io.on('connection', function (socket) {
     console.log('User connected via socket.io!');
     
     socket.on('message', function(message) {
-        console.log('Message received' + message.text);
+        console.log('Message received: ' + message.text);
         
         socket.broadcast.emit('message', message);
     })
