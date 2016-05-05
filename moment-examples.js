6821f912ccba9f11379a241a819ee528976b34e1
Paul Cutting <paul.cutting@axonvibe.com>
2016-05-05 12:28:10 +0100

    Show timestamp on UI

--- moment-examples.js
+++ moment-examples.js
@@ -12,9 +12,11 @@ var now = moment();
 // console.log(now.format('MMM Do YYYY, h:mm:a'));  //Oct 5th 2015, 6:45 pm
 
 // Convert from Unix Timestamp
-var timestamp = 1462444356417;
+var timestamp = now.valueOf(); //1462444356417;
+console.log(timestamp);
 var timestampMoment = moment.utc(timestamp);
+console.log(moment.utc(timestamp).local().format('h:mm:a'));
+
 console.log(timestampMoment.local().format('h:mm:a'));  //11:06 am
-
-
+console.log(moment.utc(timestamp).local().format('h:mm:a'));
 
