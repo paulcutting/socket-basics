b1870bce93dfa61f9f5278dbe5973ea24984af5d
Paul Cutting <paul.cutting@outlook.com>
2016-07-01 21:26:49 +0100

    Add styles for chat app

--- public/js/app.js
+++ public/js/app.js
@@ -17,13 +17,15 @@ jQuery('.room-title').text(room);
 
 socket.on('message', function (message) {
     var momentTimestamp = moment.utc(message.timestamp);
-    var $message = jQuery('.messages');
+    var $messages = jQuery('.messages');
+    var $message = jQuery('<li class="list-group-item"></li>')
     
     console.log('New message:');
     console.log(message.timestamp + ': ' + message.text);
     
     $message.append('<p><strong>' + message.name + ' ' + momentTimestamp.local().format('h:mm:a') + '</strong></p>');
     $message.append('<p>' + message.text + '</p>');
+    $messages.append($message);
 });
 
 // Handles submitting a new message
